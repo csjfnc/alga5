c17b7499c1ae84537100411397b9e0b4ccf366e3
csjfnc <38473345+csjfnc@users.noreply.github.com>
2018-08-08 15:46:28 -0300

    teste2

--- src/main/java/com/algamoney5/curso/resource/CategoriaResource.java
+++ src/main/java/com/algamoney5/curso/resource/CategoriaResource.java
@@ -39,7 +39,7 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 
 	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	//@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
 	public List<Categoria> listar(){	
 		return categoriaRepository.findAll();
 	}
